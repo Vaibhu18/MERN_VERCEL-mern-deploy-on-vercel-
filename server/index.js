c22967f5e8391383b64e3d0dac3c50193a4b1e42
vaibhav shinde <vaibhu8605@gmail.com>
2025-08-19 16:35:47 +0530

    add mongodb connection

--- server/index.js
+++ server/index.js
@@ -6,12 +6,36 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-app.get("/vaibhav", async (req, res) => {
-    return res.json({ name: "Hey Vaibhav Shinde" })
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
 })
-app.get("/onkar", async (req, res) => {
-    return res.json({ name: "Hey Onkar Shinde" })
+
+const userModel = mongoose.model("User", userSchema)
+
+mongoose.connect("mongodb+srv://vcode:vcode123@general-use.akpl2d4.mongodb.net/deployment")
+    .then(() => {
+        console.log("Mongodb is connected")
+    })
+    .catch(() => {
+        console.log("Mongodb Connection Faild")
+    })
+
+app.post("/api/register-user", async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const isUserExist = await userModel.findOne({ email });
+        if (isUserExist) return res.json({ status: false, message: "User already exist" });
+        const user = await userModel.create({ name, email, password })
+        if (user) return res.json({ status: true, message: "user registered" });
+        return res.json({ status: false, message: "user registration faild" });
+    } catch (error) {
+        return res.json({ status: false, message: error.message })
+    }
 })
+
+
 
 app.listen(3000, () => {
     console.log("Server start on port 3000")
